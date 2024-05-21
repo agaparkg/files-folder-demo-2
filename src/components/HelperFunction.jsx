fe951e55ecac255591c6a1b63983180b62d83c08
Azamat Gaparov <agaparovkg@gmail.com>
2024-05-20 19:01:46 -0700

    new changes

--- src/components/HelperFunction.jsx
+++ src/components/HelperFunction.jsx
@@ -1,18 +1,24 @@
+import { useState } from "react";
 import RowContainer from "./RowContainer";
 
 function HelperFunction({ data }) {
+  const [expand, setExpand] = useState(false);
+
+  //   const toggle = () => console.log("folder");
+  const toggle = () => setExpand(!expand);
+
   if (!data.isFolder) {
     return (
       <div className="inner1">
-        <FaFile /> {data.name}
+        <RowContainer name={data.name} type="file" />
       </div>
     );
   }
 
   return (
     <div className="inner1">
-      <RowContainer />
-      <div>
+      <RowContainer toggle={toggle} name={data.name} type="folder" />
+      <div style={{ display: expand ? "block" : "none" }}>
         {data.items &&
           data.items.map((item) => {
             return <HelperFunction data={item} />;
--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { FaFolder, FaFile } from "react-icons/fa";
 
-function RowContainer() {
+function RowContainer({ type, name, toggle }) {
   return (
-    <div className="inner2">
-      <FaFolder style={{ color: "red" }} /> {data.name}
+    <div className="inner2" onClick={toggle ? toggle : null}>
+      {type === "folder" ? <FaFolder style={{ color: "red" }} /> : <FaFile />}
+      {name}
     </div>
   );
 }
